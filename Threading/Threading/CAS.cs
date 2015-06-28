7cd7d6ceb94b12b6be869de6cff8ef2dd47af7c4
kazheko <kazheko@xaker.ru>
2015-06-28 14:37:32 +0300

    added CAS example

---
+++ Threading/Threading/CAS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Cas
+    {
+        static void Main(string[] args)
+        {
+            string x = "1", y = "2";
+            DoWithCas(ref x, temp=>temp+y);
+        }
+
+        static void Operation(ref string x, string y)
+        {
+            string temp, result;
+            do
+            {
+                temp = x;
+                result = temp + y;
+            } while (Interlocked.CompareExchange(ref x, result, temp) != temp);
+        }
+
+        static void DoWithCas<T>(ref T location, Func<T, T> func) 
+            where T : class 
+        {
+            T temp, result;
+            do
+            {
+                temp = location;
+                result = func(temp);
+            } while (Interlocked.CompareExchange<T>(ref location, result, temp) != temp);
+        }
+    }
+}
